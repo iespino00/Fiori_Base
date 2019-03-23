d099acf792033a70e5b94ec4f5b0a0b0aedba306
iespino <iespino69@gmail.com>
2019-03-23 09:52:06 -0600

    Mandando JSON  la vista

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel){
     "use strict";
     
     return Controller.extend("com.dudev.UI5C.controller.Main",{
@@ -8,9 +9,21 @@ sap.ui.define([
         /**
          * Event that will be called when the view is initialized
          */
-        onInit : function(){            
-            
-        }
+        onInit : function()
+             {            
+              this._loadTasks();
+             },
+             
+        /*Function para cargar las tareas*/
+        _loadTasks: function()
+        			{
+        	        var oController = this;
+        	
+        			$.getJSON("./model/mock/tasks.json" , function(json){
+        			   var oModel = new JSONModel(json);
+        			   oController.getView().byId("tabTasks").setModel(oModel); //Se asigna el modelo del Json a la tabla de la vista.
+        				});
+        			}
         
     });
     
