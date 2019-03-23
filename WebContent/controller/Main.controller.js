1172661c1b0cbcf4d595d96bd4f4d5eecff2cac8
iespino <iespino69@gmail.com>
2019-03-23 10:20:56 -0600

    Creando variable global a controller para validar la llamada del Json a un WS

--- WebContent/controller/Main.controller.js
+++ WebContent/controller/Main.controller.js
@@ -1,30 +1,36 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function(Controller,JSONModel){
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function(Controller, JSONModel, MessageToast){
     "use strict";
-    
-    return Controller.extend("com.dudev.UI5C.controller.Main",{
-    
-        /**
+
+        return Controller.extend("com.dudev.UI5C.controller.Main",{
+    	
+    	isMockRun: false,
+
+            /**
          * Event that will be called when the view is initialized
          */
-        onInit : function()
-             {            
-              this._loadTasks();
-             },
-             
-        /*Function para cargar las tareas*/
-        _loadTasks: function()
-        			{
-        	        var oController = this;
-        	
-        			$.getJSON("./model/mock/tasks.json" , function(json){
-        			   var oModel = new JSONModel(json);
-        			   oController.getView().byId("tabTasks").setModel(oModel); //Se asigna el modelo del Json a la tabla de la vista.
-        				});
-        			}
-        
+        onInit : function(){
+        	this._loadTasks();
+        },
+           /*Funcion para cambiar tareas*/
+           _loadTasks: function()
+           {
+	        	var oController = this;
+	        	
+	        	if(oController.isMockRun)
+	        	{
+	        		$.getJSON("./model/mock/tasks.json", function(json){
+	            		var oModel = new JSONModel(json);
+	            		oController.getView().byId("tabTasks").setModel(oModel);
+	            	});
+	        	}else{
+	        		MessageToast.show("ToDo: Agregar llamado a WS de tareas");
+	        	     }
+            }
+
+            });
+
     });
-    
-});
