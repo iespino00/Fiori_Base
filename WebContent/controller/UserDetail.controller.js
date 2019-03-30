9a6a9c2d7a9d24f348f243cfcfd69f434ecb3234
iespino <iespino69@gmail.com>
2019-03-30 13:26:12 -0600

    Copiando codigo del Controller from Instructor, donde agrego el llamado de ajax para obtener los detalles del codigo de user

--- WebContent/controller/UserDetail.controller.js
+++ WebContent/controller/UserDetail.controller.js
@@ -9,32 +9,51 @@ sap.ui.define([
                 /**
          * Event that will be called when the view is initialized
          */
-        onInit : function()
-         {
-        	
-            var oRouter = sap.ui.coreUIComponent.getRouterFor(this);
-            oRouter.getRouter("userDetailrRoute").attachPatternMatched(this.onPageLoaded, this);
-         },
-                    
-        //Evento llamado cada vez que se navega a esta vista
+        onInit : function(){
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("userDetailRoute").attachPatternMatched(this.onPageLoaded, this);
+        },
+
+                //Evento llamado cada vez que se navega a esta vista
         onPageLoaded : function(oEvent)
         {
         	//Get user code from url
         	var userCode = oEvent.getParameter("arguments").code;
-        	this._loadDetail(userCode);
-        },   
-                    
-        _loadDetail : function(userCode)
-        {
         	
-        },            
-        
-        onBack : function()
+        	this._loadDetail(userCode);
+        	
+        },
+
+         _loadDetail : function(userCode)
          {
-           var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-           oRouter.navTo("mainRoute",null,true); //No conservar el historial
-         }
-    
-     });
+        	var oController = this;
+        	$.ajax({
+                type: "GET",
+                url: "http://webide.dudev.mx/dudev/webide/pro/com.dudev.SDY/appi",
+                datatype: "json",
+                cache: "false",
+                data: {
+                    dvsn : "TASK",
+                    opn  : "GTBU",
+                    c    : userCode
+                }
+            }).done(function(oResponse){
+            	var oModel = new JSONModel(oResponse);
+            	oController.getView().setModel(oModel);
+            }).fail(function(){
+                //Handle errors
+            }).always(function(){
+            	//Reset Values
+            });
+        },
+
+       onBack : function()
+       {
+        	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        	oRouter.navTo("mainRoute", null, true);/*No conservar historial*/
+        }
+
+
+                    });
 
     });
