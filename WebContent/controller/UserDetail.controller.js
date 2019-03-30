0e9a8479ce187379649ecfd5acfa777a2a6b172c
iespino <iespino69@gmail.com>
2019-03-30 12:44:32 -0600

    Se creo el controller para la vista de detalle, y se agrego el codigo para este nuevo controller

---
+++ WebContent/controller/UserDetail.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel){
+    "use strict";
+
+        return Controller.extend("com.dudev.UI5C.controller.UserDetail",{
+
+                /**
+         * Event that will be called when the view is initialized
+         */
+        onInit : function(){
+
+                    },
+    });
+
+    });
